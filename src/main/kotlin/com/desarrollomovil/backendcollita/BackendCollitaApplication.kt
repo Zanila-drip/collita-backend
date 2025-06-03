40c6ac06d17b940d19e47ef7e3e4e3d6cb8681ad
Leonardo <217o02108@itsx.edu.mx>
2025-06-03 13:07:28 -0600

    v0.1

---
+++ src/main/kotlin/com/desarrollomovil/backendcollita/BackendCollitaApplication.kt
@@ -0,0 +1,11 @@
+package com.desarrollomovil.backendcollita
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendCollitaApplication
+
+fun main(args: Array<String>) {
+    runApplication<BackendCollitaApplication>(*args)
+}
---
+++ src/test/kotlin/com/desarrollomovil/backendcollita/BackendCollitaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.desarrollomovil.backendcollita
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendCollitaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
