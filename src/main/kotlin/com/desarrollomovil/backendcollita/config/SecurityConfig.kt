42d3567fc03819cb9b29dd57c0234056b391ce44
Leonardo <217o02108@itsx.edu.mx>
2025-06-04 08:59:07 -0600

    v0.2

--- src/main/kotlin/com/desarrollomovil/backendcollita/config/SecurityConfig.kt
+++ src/main/kotlin/com/desarrollomovil/backendcollita/config/SecurityConfig.kt
@@ -39,10 +39,10 @@ class SecurityConfig {
     @Bean
     fun corsConfigurationSource(): CorsConfigurationSource {
         val configuration = CorsConfiguration()
-        configuration.allowedOrigins = listOf("http://localhost:8080", "http://localhost:3000", "*")
+        configuration.allowedOrigins = listOf("http://localhost:8080", "http://localhost:3000", "https://collita-backend-production.up.railway.app")
         configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
         configuration.allowedHeaders = listOf("*")
-        configuration.allowCredentials = true
+        configuration.allowCredentials = false
         
         val source = UrlBasedCorsConfigurationSource()
         source.registerCorsConfiguration("/**", configuration)
